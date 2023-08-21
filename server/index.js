9e37c89da3bce2e4c570db2c06d95319bad7e14a
Sukhpreet Singh <72423196+sukhpreet-singh1@users.noreply.github.com>
2023-08-22 00:20:51 +0530

    ufff

--- server/index.js
+++ server/index.js
@@ -2,10 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "./models/User.js";
-import { handleError } from "./error.js";
+
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auths.js";
 import tweetRoutes from "./routes/tweets.js";
@@ -28,31 +25,11 @@ const connect = () => {
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", async (req, res, next) => {
-  try {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
-    const newUser = new User({ ...req.body, password: hash });
-    await newUser.save();
-    const token = jwt.sign({ id: newUser._id }, "1234567sadvlbhj");
-    const { password, ...othersData } = newUser._doc;
-    res
-      .cookie("access_token", token, {
-        httpOnly: true,
-      })
-      .status(200)
-      .json(othersData);
-  } catch (err) {
-    next(err);
-  }
-});
+app.use("/api/auth", authRoutes);
 app.use("/api/tweets", tweetRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api", (req, res) => {
-  res.json({ messsage: "helloasdv" });
-});
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   connect();
   console.log("Listening to port 8000");
 });
