282d1d74df46f076e2cdd86e63fc4dee55f10aed
Sukhpreet Singh <72423196+sukhpreet-singh1@users.noreply.github.com>
2023-08-21 23:39:41 +0530

    Update LeftSidebar.jsx

--- client/src/components/LeftSidebar/LeftSidebar.jsx
+++ client/src/components/LeftSidebar/LeftSidebar.jsx
@@ -18,7 +18,7 @@ const LeftSidebar = () => {
   };
 
   return (
-    <div className="flex flex-col fixed top-14 h-full md:h-[90vh] justify-between mr-6">
+    <div className="flex flex-col fixed top-20 h-full md:h-[90vh] justify-between mr-6">
       <div className="mt-6 flex flex-col space-y-4">
         <Link to="/">
           <div className="flex items-center space-x-6 px-2 py-2 hover:bg-gray-50 rounded-full cursor-pointer">
